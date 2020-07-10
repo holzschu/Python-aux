32050f394444cc7178e4d49d0011a87525881f64
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-07-10 21:34:12 +0200

    Patch for libffi (temporary)

---
+++ libpng/libpng.h
@@ -0,0 +1,18 @@
+//
+//  libpng.h
+//  libpng
+//
+//  Created by Nicolas Holzschuch on 10/07/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for libpng.
+FOUNDATION_EXPORT double libpngVersionNumber;
+
+//! Project version string for libpng.
+FOUNDATION_EXPORT const unsigned char libpngVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libpng/PublicHeader.h>
+
+
