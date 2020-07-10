b386a1bad388e22544b78b3dde830c1a80f885d9
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-07-10 20:49:49 +0200

    Added libffi

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Python-aux",
     products: [
-        .library(name: "Python-aux", targets: ["libpng"])
+        .library(name: "Python-aux", targets: ["libpng", "libffi"])
     ],
     dependencies: [
     ],
@@ -14,6 +14,12 @@ let package = Package(
             name: "libpng",
             url: "https://github.com/holzschu/Python-aux/releases/download/1.0/libpng.xcframework.zip",
             checksum: "d88a7fe745950eec19fc6f69321b853ee98eda75a9730d64a46d94c0de4d06b9"
+        ),
+        .binaryTarget(
+            name: "libffi",
+            url: "https://github.com/holzschu/Python-aux/releases/download/1.0/libffi.xcframework.zip",
+            checksum: "f75b0388d255e38f861daf7012d0842842a33594619312db7e34bc311c815da8"
         )
     ]
 )
+
