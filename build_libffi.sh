ea009d58514b57158d98df2345e1234d5025c38f
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-08-25 16:50:45 +0200

    Updated buuld_libffi

--- build_libffi.sh
+++ build_libffi.sh
@@ -8,6 +8,11 @@ pushd libffi
 # patch -p1 < ../libffi.patch 
 xcodebuild -project libffi.xcodeproj -target libffi-iOS -sdk iphoneos -arch arm64 -configuration Debug -quiet
 xcodebuild -project libffi.xcodeproj -target libffi-iOS -sdk iphonesimulator -arch x86_64 -configuration Debug -quiet
+# Python also need ffi_common.h and fficonfig.h
+cp build_iphoneos-arm64/fficonfig.h build/Debug-iphoneos/include/ffi/
+cp include/ffi_common.h build/Debug-iphoneos/include/ffi/ 
+cp build_iphonesimulator-x86_64/fficonfig.h build/Debug-iphonesimulator/include/ffi/
+cp include/ffi_common.h build/Debug-iphonesimulator/include/ffi/
 popd 
 # then, merge them into XCframeworks:
 framework=libffi
@@ -16,5 +21,4 @@ xcodebuild -create-xcframework \
 	-library $framework/build/Debug-iphoneos/libffi.a -headers $framework/build/Debug-iphoneos/include \
 	-library $framework/build/Debug-iphonesimulator/libffi.a -headers $framework/build/Debug-iphonesimulator/include \
 	-output $framework.xcframework
-
 
