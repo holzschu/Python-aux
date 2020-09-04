63c7e849d6953a957956a8ffdd35b6773e263304
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-09-04 13:43:19 +0200

    Added crypto and openssl

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Python-aux",
     products: [
-        .library(name: "Python-aux", targets: ["libpng", "libffi", "libzmq", "openblas", "freetype", "harfbuzz"])
+        .library(name: "Python-aux", targets: ["libpng", "libffi", "libzmq", "openblas", "freetype", "harfbuzz", "crypto", "openssl"])
     ],
     dependencies: [
     ],
@@ -13,46 +13,60 @@ let package = Package(
         .binaryTarget(
             name: "libpng",
             url: "https://github.com/holzschu/Python-aux/releases/download/1.0/libpng.xcframework.zip",
-            checksum: "c699c76f93e91142832a342fe59d4012ceabc728694a89deeaee14ede4f3b133"
+            checksum: "676f8de88c5e2db7195d792ab5a3c028353453a367ebf446bf3dbf1cc0f7619a"
         ),
         .binaryTarget(
             name: "libffi",
             url: "https://github.com/holzschu/Python-aux/releases/download/1.0/libffi.xcframework.zip",
-            checksum: "1fce5ee5fed2a864d7047082ec013751da78107bc58fe9b473810788a557c273"
+            checksum: "b87aab474a60a6906194e2c4325eb7e75f6b98fc405cc6f2a93b2aa0d8be9b7e"
         ),
         .binaryTarget(
             name: "libzmq",
             url: "https://github.com/holzschu/Python-aux/releases/download/1.0/libzmq.xcframework.zip",
-            checksum: "80ea3ea3d369445b1abb0c61bde304e24695cb95f5808944ac6dabc634f4076a"
+            checksum: "f39faf8387951e77cf4c4a7e430a479055e6cf88638777e968679f996829b0f4"
         ),
         .binaryTarget(
             name: "openblas",
             url: "https://github.com/holzschu/Python-aux/releases/download/1.0/openblas.xcframework.zip",
-            checksum: "d326c7b6731ddf4b541900e37752c12a760eca848d2fdffd33897d7ad700befa"
+            checksum: "29d2e5a60901bc0461213ac6db595691094e2c79418ac6927d24b0fb15b0d7c6"
         ),
         .binaryTarget(
             name: "freetype",
             url: "https://github.com/holzschu/Python-aux/releases/download/1.0/freetype.xcframework.zip",
-            checksum: "e281c8f72f512652860385cb68d246ba787d134cc04589b6fca33608f449bd91"
+            checksum: "58e3ce5d2c81f799bee4e20b6da883adc394f31318e7975e30b7d5b22fe2be28"
         ),
         .binaryTarget(
             name: "harfbuzz",
             url: "https://github.com/holzschu/Python-aux/releases/download/1.0/harfbuzz.xcframework.zip",
-            checksum: "4b4e6672162d4491562eed5ef6e06a6de25d0f2e07ab8b095986a1921b497a07"
+            checksum: "c580567dbc47433f1aae312a62bdaaf97894cf9a70ce75e1d3ac31b5c2a2cd18"
+        ),
+        .binaryTarget(
+            name: "crypto",
+            url: "https://github.com/holzschu/Python-aux/releases/download/1.0/crypto.xcframework.zip",
+            checksum: "5caf39e44187d99cd9c89a4fb7a0b63439e29fc67437fe793e5b4d620c730bb9"
+        ),
+        .binaryTarget(
+            name: "openssl",
+            url: "https://github.com/holzschu/Python-aux/releases/download/1.0/openssl.xcframework.zip",
+            checksum: "3fcca6d84391c7bc927d3e606735560ce55b07710ce100f2bbdab166a7370473"
         )
     ]
 )
 /*
 libpng
-c699c76f93e91142832a342fe59d4012ceabc728694a89deeaee14ede4f3b133
+676f8de88c5e2db7195d792ab5a3c028353453a367ebf446bf3dbf1cc0f7619a
 libffi
-1fce5ee5fed2a864d7047082ec013751da78107bc58fe9b473810788a557c273
+b87aab474a60a6906194e2c4325eb7e75f6b98fc405cc6f2a93b2aa0d8be9b7e
 libzmq
-80ea3ea3d369445b1abb0c61bde304e24695cb95f5808944ac6dabc634f4076a
+f39faf8387951e77cf4c4a7e430a479055e6cf88638777e968679f996829b0f4
 openblas
-d326c7b6731ddf4b541900e37752c12a760eca848d2fdffd33897d7ad700befa
+29d2e5a60901bc0461213ac6db595691094e2c79418ac6927d24b0fb15b0d7c6
 freetype
-e281c8f72f512652860385cb68d246ba787d134cc04589b6fca33608f449bd91
+58e3ce5d2c81f799bee4e20b6da883adc394f31318e7975e30b7d5b22fe2be28
 harfbuzz
-4b4e6672162d4491562eed5ef6e06a6de25d0f2e07ab8b095986a1921b497a07
+c580567dbc47433f1aae312a62bdaaf97894cf9a70ce75e1d3ac31b5c2a2cd18
+crypto
+5caf39e44187d99cd9c89a4fb7a0b63439e29fc67437fe793e5b4d620c730bb9
+openssl
+3fcca6d84391c7bc927d3e606735560ce55b07710ce100f2bbdab166a7370473
 */
