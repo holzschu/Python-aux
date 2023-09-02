44332fd88fc75fceaba0fcc404bc5cda6af198db
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2023-09-02 09:15:57 +0200

    Updated list of packages

--- compute_checksums.sh
+++ compute_checksums.sh
@@ -1,4 +1,24 @@
-for framework in libpng libffi libzmq openblas freetype harfbuzz crypto openssl libjpeg libtiff libxslt libexslt libfftw3 libfftw3_threads
+for framework in libpng \
+	libffi \
+	libzmq \
+	openblas \
+	freetype \
+	harfbuzz \
+	crypto \
+	openssl \
+	libjpeg \
+	libtiff \
+	libxslt \
+	libexslt \
+	libfftw3 \
+	libfftw3_threads \
+	libspatialindex_c \
+	libspatialindex \
+	libgdal \
+	libproj \
+	libgeos_c \
+	libgeos \
+	liblzma
 do
    echo $framework
    rm -f $framework.xcframework.zip
